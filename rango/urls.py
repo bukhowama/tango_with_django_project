511e48b212c77fc2da65581f20e095ad583712ad
Maryam Bukhowa <bukhowa.ma@gmail.com>
2019-01-25 14:31:45 +0000

    fourth commit

--- rango/urls.py
+++ rango/urls.py
@@ -3,5 +3,11 @@ from rango import views
 
 urlpatterns = [
         url(r'^$', views.index, name='index'),
+
+        url(r'^index/', views.index, name='index'),
+        #is this correct??
+
         url(r'^about/', views.about, name='about'),
 ]
+
+
--- rango/views.py
+++ rango/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
 
-    return HttpResponse("Rango says hey there partner!  <br/> <a href=' /rango/about/'>About</a>")
 
 def about(request) :
-        return HttpResponse("Rango says here is the about page <br/> <a href=' /rango/'>Index</a>")
+    context_dict = {'message': " This tutorial has been put together by Maryam"}
+    return render(request, 'rango/about.html', context=context_dict)

e66c069727baf221cebce82cbd8bb39ff713f71f
Maryam Bukhowa <bukhowa.ma@gmail.com>
2019-01-27 12:48:03 +0000

    sixth commit

--- rango/urls.py
+++ rango/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
         #is this correct??
 
         url(r'^about/', views.about, name='about'),
+        url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+            views.show_category, name='show_category'),
+
 ]
 
 
